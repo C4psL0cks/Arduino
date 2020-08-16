977d7a6745f9a718a96157a3ef6e36fdd06d3aab
DevCoding <fenixalone@hotmail.com>
2020-08-16 18:40:02 +0700

    by dev codings

--- Ardunio + voltage/Ardunio_voltage.ino
+++ Ardunio + voltage/Ardunio_voltage.ino
@@ -1,24 +1,25 @@
+
 // 0-25v voltage sensor is connected with the analog pin A0 of the arduino
 int Vsensor = A0;
 
 //For 0-25v voltage sensor
-float correctionfactor = 3;
+float correctionfactor = 5.14;
 float vout = 0.0;
 float vin = 0.0;
 
 // two resistors 30K and 7.5k ohm
-float R1 = 30000;
-float R2 = 7500;
+float R1 = 4700.0;
+float R2 = 2200.0;
 int value = 0;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 }
 
 void loop() {
 
   value = analogRead(Vsensor);
-  vout = (value * 3.0) / 1023.0; 
+  vout = (value * correctionfactor) / 1024.0; 
   //Serial.println(value);
   vin = vout / (R2 / (R1 + R2));
   //vin = vin - correctionfactor;
