f8f97a33d469a3b2c89517413b916f848a09aef1
c4psl0cks <ox03795@hotmail.com>
2020-09-08 16:59:37 +0700

    update

--- NRF24L01-Module/send/send.ino
+++ NRF24L01-Module/send/send.ino
@@ -1,20 +1,26 @@
 #include <SPI.h>
 #include <nRF24L01p.h>
 nRF24L01p transmitter(7, 8); //CSN,CE
+String message;
+
 void setup() {
+
   delay(150);
+
   Serial.begin(115200);
   SPI.begin();
   SPI.setBitOrder(MSBFIRST);
   transmitter.channel(90); // ตั้งช่องความถี่ให้ตรงกัน
   transmitter.TXaddress("ALL"); // ตั้งชื่อตำแหน่งให้ตรงกัน ชื่อตั้งได้สูงสุด 5 ตัวอักษร
   transmitter.init();
+  Serial.println("RUN...Sender");
+
 }
-String message;
+
 void loop() {
   int vr = analogRead(A0);
   Serial.println("VR:" + String(vr));
-  transmitter.txPL(String(vr)); // ค่าที่ต้องการส่ง
+  transmitter.txPL(vr); // ค่าที่ต้องการส่ง
   transmitter.send(FAST); // สั่งให้ส่งออกไป
-  delay(200);
-}  
+  //  delay(50);
+}
